037803ae3678b81e6105de2f058009463b06e428
Cela Pablo <pablooandress9@gmail.com>
2023-02-23 00:05:22 -0300

    removes extra code - adds info to summary text

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-import 'classlist-polyfill';
 import Promise from 'bluebird';
 import summaryText from 'raw-loader!./summary.txt';
-import footerHTML from 'raw-loader!./footer.html';
-let styleText = [0, 1].map((i) => require('raw-loader!./styles' + i + '.css').default);
 import preStyles from 'raw-loader!./prestyles.css';
-import { writeSimpleChar } from './lib/writeChar';
-import getPrefix from './lib/getPrefix';
 
 // Vars that will help us get er done
-const isDev = window.location.hostname === 'localhost';
-let speed = isDev ? 16 : 32;
-let style, summaryEl;
-let animationSkipped = false, done = false, paused = false;
-let browserPrefix;
+let speed = window.location.hostname === 'localhost' ? 16 : 32;
+let skipEl, summaryEl;
+let animationSkipped = false, done = false;
 
 // Wait for load to get started.
 document.addEventListener("DOMContentLoaded", function() {
-  getBrowserPrefix();
-  populateFooter();
   getEls();
   createEventHandlers();
   startAnimation();
@@ -38,16 +29,9 @@ async function startAnimation() {
   }
 }
 
-// Skips all the animations.
 async function surprisinglyShortAttentionSpan() {
   if (done) return;
   done = true;
-  let txt = styleText.join('\n');
-
-  // The work-text animations are rough
-  // style.textContent = "#work-text * { " + browserPrefix + "transition: none; }";
-  style.textContent += txt;
-
   // There's a bit of a scroll problem with this thing
   let start = Date.now();
   while(Date.now() - 1000 > start) {
@@ -56,15 +40,13 @@ async function surprisinglyShortAttentionSpan() {
   }
 }
 
-/**
- * Helpers
- */
-
+/** Helpers */
 let endOfSentence = /[\.\?\!]\s$/;
-let comma = /\D[\,]\s$/;
 let endOfBlock = /[^\/]\n\n$/;
+let initCommand = /[\$]/;
+let command = false, endBlock = false;
 
-async function writeTo(el, message, index, interval, charsPerInterval){
+async function writeTo(el, message, index, interval, charsPerInterval) {
   if (animationSkipped) {
     // Lol who needs proper flow control
     throw new Error('SKIP IT');
@@ -82,28 +64,23 @@ async function writeTo(el, message, index, interval, charsPerInterval){
     let thisInterval = (speed === interval) ? interval : speed;
 
     let thisSlice = message.slice(index - 2, index + 1);
-    if (comma.test(thisSlice)) thisInterval = interval * 10;
-    if (endOfBlock.test(thisSlice)) thisInterval = interval * 10;
-    if (endOfSentence.test(thisSlice)) thisInterval = interval * 20;
 
-    do {
-      await Promise.delay(thisInterval);
-    } while(paused);
+    if(initCommand.test(thisSlice)) command = true;
+    if (endOfSentence.test(thisSlice)) thisInterval = interval * 20;
+    if (endOfBlock.test(thisSlice)) {
+      endBlock = true;
+      thisInterval = interval * 10
+    };
+    if(command && endBlock) {
+      command = false;
+      endBlock = false;
+      thisInterval = interval * 35;
+    }
+
+    await Promise.delay(thisInterval);
 
     return writeTo(el, message, index, interval, charsPerInterval);
   }
-}
-
-//
-// Older versions of major browsers (like Android) still use prefixes. So we figure out what that prefix is
-// and use it.
-//
-function getBrowserPrefix() {
-  // Ghetto per-browser prefixing
-  browserPrefix = getPrefix(); // could be empty string, which is fine
-  styleText = styleText.map(function(text) {
-    return text.replace(/-webkit-/g, browserPrefix);
-  });
 }
 
 //
@@ -114,30 +91,24 @@ function getEls() {
   let preStyleEl = document.createElement('style');
   preStyleEl.textContent = preStyles;
   document.head.insertBefore(preStyleEl, document.getElementsByTagName('style')[0]);
-
-  // El refs
-  style = document.getElementById('style-tag');
   summaryEl = document.getElementById('summary-text');
-}
-
-//
-// Create links in footer.
-//
-function populateFooter() {
-  let footer = document.getElementById('footer');
-  footer.innerHTML = footerHTML;
+  skipEl = document.getElementById('skip-animation');
 }
 
 //
 // Create basic event handlers for user input.
 //
 function createEventHandlers() {
-  // Skip anim on press esc
   document.addEventListener('keydown', function(e) {
     if (e.key === 'Escape') {
       summaryEl.innerHTML = summaryText;
       summaryEl.scrollTop = summaryEl.scrollHeight;
       animationSkipped = true;
+      skipEl.setAttribute('style', 'color:transparent;');
     }
   });
 }
+
+function writeSimpleChar(el, char) {
+  el.innerHTML += char;
+};
